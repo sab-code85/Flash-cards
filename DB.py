c33b393fb6c86feb3d2a605bf8bad7a56cd382b6
Admin <sab-design@ya.ru>
2026-01-04 14:24:14 +0300

    4.01.2026 - 1

---
+++ DB.py
@@ -0,0 +1,9 @@
+def create_DB(name_DB):
+    import sqlite3
+    base = sqlite3.connect(name_DB)
+    cur = base.cursor
+
+
+def save_to_BD():
+    '''save card to data base'''
+    pass
---
+++ main.py
@@ -0,0 +1,25 @@
+# # базовый класс для карточки
+# class Card():
+#     def __init__(self):
+#         self.word = ''
+#         self.translate = ''
+#         self.memorize = 100
+
+from DB import *
+from tkinter import *
+
+
+# описание окна
+main_window = Tk()
+main_window.title('Флеш-карты')
+main_window.geometry('500x500+1000+200')
+word_field = Entry(width=30)  # окно ввода слова
+translate_field = Entry(width=30)  # окно ввода слова перевода
+Saver_button = Button(text='Сохранить')
+Saver_button.bind('<Button-1>', save_to_BD())
+
+# вывод на экран
+word_field.place(x=100, y=100)
+translate_field.place(x=100, y=200)
+Saver_button.place(x=100, y=300)
+main_window.mainloop()
